55442051917b9e962e70ef43cfd8346e977a2f90
Borkau <borkau.box@gmail.com>
2020-08-29 18:59:08 +0500

    Create new test project

---
+++ src/main/java/com/example/Maat/MaatApplication.java
@@ -0,0 +1,13 @@
+package com.example.Maat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaatApplication.class, args);
+	}
+
+}
