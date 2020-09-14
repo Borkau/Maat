ee1db15bb42270366f18c445031b80c11c7c81b4
Borkau <borkau.box@gmail.com>
2020-09-14 20:57:48 +0500

    Remake controller to @Modelattribute

--- src/main/java/com/example/Maat/controller/SecurityController.java
+++ src/main/java/com/example/Maat/controller/SecurityController.java
@@ -2,44 +2,26 @@ package com.example.Maat.controller;
 
 
 import com.example.Maat.dto.SecurityDto;
+import com.example.Maat.entity.Security;
 import com.example.Maat.service.SecurityService;
+import com.example.Maat.serviceImpl.SecurityServiceImpl;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/security",method = RequestMethod.POST,produces = "application/json; charset=utf-8")
 @AllArgsConstructor
 @Log
+@CrossOrigin
 public class SecurityController {
 
-    private final SecurityService securityService;
 
-    @PostMapping("/save")
-    public SecurityDto saveSecurity(@RequestBody SecurityDto securityDto) {
-        log.info("Handling save security: " + securityDto);
-        return securityService.saveSecurity(securityDto);
-    }
 
-    @GetMapping("/findAll")
-    public List<SecurityDto> findAll() {
-        log.info("Handling all securities request");
-        return securityService.findAll();
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteSecurity(@PathVariable Integer id) {
-        log.info("Handling delete security request: " + id);
-        securityService.deleteSecurity(id);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/findByName")
-    public SecurityDto findByName(@RequestParam String name) {
-        log.info("Handling find by security request: " + name);
-        return securityService.findByName(name);
-    }
 }
